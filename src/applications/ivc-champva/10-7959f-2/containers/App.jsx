c02a0eef70764c166ab5b73dd4529a5dc9a12877
Matt Long <6738544+longmd@users.noreply.github.com>
2026-03-24 15:11:33 -0400

    10-7959f-2 | Add Datadog RUM config (#43423)

--- src/applications/ivc-champva/10-7959f-2/containers/App.jsx
+++ src/applications/ivc-champva/10-7959f-2/containers/App.jsx
@@ -1,14 +1,50 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
+import environment from 'platform/utilities/environment';
 import { DowntimeNotification } from 'platform/monitoring/DowntimeNotification';
 import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
+import { useBrowserMonitoring } from 'platform/monitoring/Datadog';
 import formConfig from '../config/form';
+
+const EXCLUDED_DOMAINS = [
+  'resource.digital.voice.va.gov',
+  'browser-intake-ddog-gov.com',
+  'google-analytics.com',
+  'eauth.va.gov',
+  'api.va.gov',
+];
+
+const BROWSER_MONITORING_PROPS = {
+  toggleName: 'form107959f2BrowserMonitoringEnabled',
+  applicationId: '66f9f75c-afb7-4ebe-82c7-db75c5bb9e29',
+  clientToken: 'pub84d00359746e2012044f5b265f458045',
+  service: 'ivc-fmp-10-7959f-2',
+  version: '1.0.0',
+  env: environment.vspEnvironment(),
+  sessionSampleRate: 100,
+  sessionReplaySampleRate: 100,
+  trackUserInteractions: true,
+  trackFrustrations: true,
+  trackResources: true,
+  trackLongTasks: true,
+  defaultPrivacyLevel: 'mask-user-input',
+  beforeSend: ({ action, type, resource }) => {
+    // eslint-disable-next-line no-param-reassign
+    if (action?.type === 'click') action.target.name = 'Form item';
+    return !(
+      type === 'resource' &&
+      EXCLUDED_DOMAINS.some(domain => resource.url.includes(domain))
+    );
+  },
+};
 
 const App = ({ location, children }) => {
   const isAppLoading = useSelector(state =>
     Boolean(state.featureToggles?.loading || state.user?.profile?.loading),
   );
+
+  useBrowserMonitoring(BROWSER_MONITORING_PROPS);
 
   return isAppLoading ? (
     <va-loading-indicator
