598f2521d9186bfa748caafe7106bdc0d920cfd1
Haritha Cheruvu <haritha.cheruvu@va.gov>
2026-03-31 13:10:56 -0400

    ICAMNG-10523: Update AccessVA widget to support staging URLs (#43634)
    * ICAMNG-10523: Update AccessVA widget to support staging URLs
    
    * ICAMNG-10523: Update AccessVA widget to support staging URLs

--- src/applications/static-pages/accessva-tools-widget/AccessVAToolsWidget.jsx
+++ src/applications/static-pages/accessva-tools-widget/AccessVAToolsWidget.jsx
@@ -1,27 +1,13 @@
 import React from 'react';
 import { VaAccordion } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
-import { ACCESSVA_LINKS } from './AccessVALinks';
+import { getAccessVALinks } from './AccessVALinks';
 import './accessva-tools-widget.scss';
 
 export function AccessVAToolsWidget() {
+  // Get environment-appropriate links (staging or production)
+  const accessVALinks = getAccessVALinks();
   return (
     <div className="accessva-tools-widget">
-      {/* Breadcrumbs */}
-      <nav aria-label="Breadcrumb" className="vads-u-margin-bottom--2">
-        <ul className="va-nav-breadcrumbs-list">
-          <li>
-            <a href="https://www.va.gov">VA.gov home</a>
-          </li>
-          <li>AccessVA tools and services</li>
-        </ul>
-      </nav>
-
-      {/* Page Header */}
-      <h1 className="vads-u-margin-bottom--2">AccessVA tools and services</h1>
-      <p className="va-introtext vads-u-margin-bottom--4">
-        Select your category to sign in to VA online services.
-      </p>
-
       {/* Accordion */}
       <VaAccordion>
         <va-accordion-item
@@ -29,7 +15,7 @@ export function AccessVAToolsWidget() {
           id="veterans-accordion"
         >
           <ul className="usa-list">
-            {ACCESSVA_LINKS.veterans.map((link, index) => (
+            {accessVALinks.veterans.map((link, index) => (
               <li key={index}>
                 <a href={link.url} className="vads-u-color--link-default">
                   {link.text}
@@ -44,7 +30,7 @@ export function AccessVAToolsWidget() {
           id="business-accordion"
         >
           <ul className="usa-list">
-            {ACCESSVA_LINKS.business.map((link, index) => (
+            {accessVALinks.business.map((link, index) => (
               <li key={index}>
                 <a href={link.url} className="vads-u-color--link-default">
                   {link.text}
