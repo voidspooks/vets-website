f6dedb56af42a82e65d047ab223f0e0a25df2732
Chris Kim <42885441+chriskim2311@users.noreply.github.com>
2026-04-20 09:31:29 -0700

    VACMS-23528 DUW Content Changes to step 1 (#44124)
    * changes to step 1
    
    * more changes to step one
    
    * updates
    
    * updates
    
    * updates
    
    * updates from PR
    
    * fix regression in logic

--- src/applications/discharge-wizard/components/resultsComponents/StepOne.jsx
+++ src/applications/discharge-wizard/components/resultsComponents/StepOne.jsx
@@ -1,185 +1,238 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import AlertMessage from './AlertMessage';
-import { determineBoardObj, determineFormData } from '../../helpers';
+import { determineFormData } from '../../helpers';
 
 import { SHORT_NAME_MAP, RESPONSES } from '../../constants/question-data-map';
-import { DRB } from '../../constants';
 
 const StepOne = ({ formResponses }) => {
   const reason = formResponses[SHORT_NAME_MAP.REASON];
-  const specialReason = [
-    RESPONSES.REASON_PTSD,
-    RESPONSES.REASON_TBI,
-    RESPONSES.REASON_SEXUAL_ASSAULT,
-  ].includes(reason);
-  const boardToSubmit = determineBoardObj(formResponses);
 
-  const reasonsObj = {
-    [RESPONSES.REASON_PTSD]: {
-      name: 'PTSD or other mental health conditions',
-      type: 'condition',
-    },
-    [RESPONSES.REASON_TBI]: {
-      name: 'TBI',
-      type: 'condition',
-    },
-    [RESPONSES.REASON_SEXUAL_ASSAULT]: {
-      name: 'sexual assault',
-      type: 'experience',
-    },
-  };
+  const { num: formNumber, link: formLink } = determineFormData(formResponses);
+  let formTitle = `Form ${formNumber}`;
+  const formFileName = formLink.split('/').pop();
+  if ([293, 149].includes(formNumber)) {
+    formTitle = `DD Form ${formNumber}`;
+  }
 
-  const strongCaseTips = () => {
-    if (specialReason) {
-      const { name, type } = reasonsObj[reason];
+  const header = `Download and fill out DD Form ${formNumber}`;
+
+  const renderItemDetailsIntro = () => {
+    const line14Text =
+      'In item 14, make sure to give a thorough answer to why you’re applying for a discharge upgrade or correction.';
+    if (reason === RESPONSES.REASON_PTSD || reason === RESPONSES.REASON_TBI) {
       return (
         <>
           <p>
-            For discharges related to {name}, be sure to answer these questions
-            to make the strongest case:
+            {line14Text} It’s important to explain how your service was
+            impacted. Many Veterans attach extra pages to answer this question.
+            Consider answering these questions in your response:
           </p>
           <ul>
             <li>
-              Did you have {type === 'experience' ? 'an' : 'a'} {type} that may
-              explain or contribute to the discharge?
-            </li>
-            <li>
-              Did that {type}{' '}
-              {reason === RESPONSES.REASON_SEXUAL_ASSAULT
-                ? 'happen'
-                : 'start or get worse'}{' '}
-              during your military service?
-            </li>
-            <li>
-              Why does the {type} directly explain or contribute to the
+              Did you have a condition that may explain or contribute to the
               discharge?
             </li>
             <li>
-              Why does the {type} carry more weight than any other reasons you
+              Did that condition start or get worse during your military
+              service?
+            </li>
+            <li>
+              How did the condition directly explain or contribute to the
+              discharge?
+            </li>
+            <li>
+              How did the condition carry more weight than any other reasons you
               may have been discharged for?
             </li>
           </ul>
         </>
       );
     }
+    if (reason === RESPONSES.REASON_SEXUAL_ASSAULT) {
+      return (
+        <>
+          <p>
+            {line14Text} It’s important to explain how your service was
+            impacted. Many Veterans attach extra pages to answer this question.
+            Consider answering these questions in your response:
+          </p>
+          <ul>
+            <li>Did that experience happen during your military service?</li>
+            <li>
+              How did the experience directly explain or contribute to the
+              discharge?
+            </li>
+            <li>
+              How did the experience carry more weight than any other reasons
+              you may have been discharged for?
+            </li>
+          </ul>
+        </>
+      );
+    }
+    if (
+      reason === RESPONSES.REASON_SEXUAL_ORIENTATION ||
+      reason === RESPONSES.REASON_UNJUST ||
+      reason === RESPONSES.REASON_ERROR
+    ) {
+      return (
+        <>
+          <p>
+            {line14Text} It’s important to explain how your service was
+            impacted. Many Veterans attach extra pages to answer this question.
+          </p>
+        </>
+      );
+    }
+    if (reason === RESPONSES.REASON_DD215_UPDATE_TO_DD214) {
+      return (
+        <>
+          <p>
+            {line14Text} You should explain why you need a new DD214, including
+            any problems you face when you have to show both the DD214 and the
+            DD215. Many Veterans attach extra pages to answer this question.
+          </p>
+        </>
+      );
+    }
+    if (reason === RESPONSES.REASON_TRANSGENDER) {
+      return (
+        <>
+          <p>{line14Text}</p>
+        </>
+      );
+    }
     return null;
   };
 
-  const dd214Tips = (
-    <ul>
-      <li>
-        Pay special attention to item 14, which asks for the reason for your
-        change. Here you should explain why you need a new DD214, including any
-        problems you face when you have to show both the DD214 and the DD215.
-        You may want to consider attaching additional pages to fully answer this
-        question.
-      </li>
-    </ul>
-  );
-
-  const nonDd2014Tips = (
-    <ul>
-      <li>
-        Pay special attention to item 14, which asks for the reason for your
-        change. Most Veterans attach additional pages to answer this question.{' '}
-        {strongCaseTips()}
-      </li>
-      {[
-        RESPONSES.PREV_APPLICATION_BCMR,
-        RESPONSES.PREV_APPLICATION_BCNR,
-      ].includes(formResponses[SHORT_NAME_MAP.PREV_APPLICATION_TYPE]) && (
-        <li>
-          Because you’re applying for reconsideration of a previous application,
-          you’ll need to enter the previous application number in Item 11b.{' '}
-          <strong>Note:</strong> You’re generally only eligible for
-          reconsideration if you have new evidence to present that wasn’t
-          available when you applied last time. Make sure you’re clear about
-          exactly what that new evidence is. Additionally, changes in DOD
-          policy, like the new consideration guidelines for PTSD, TBI, and
-          sexual assault or harassment, can qualify you for reconsideration.
-        </li>
-      )}
-      {formResponses[SHORT_NAME_MAP.REASON] ===
-        RESPONSES.REASON_SEXUAL_ASSAULT && (
-        <li>
-          <strong>Note:</strong> For upgrades related to sexual assault or
-          harassment, you do not need to prove the original assault or
-          harassment occurred—meaning if you didn’t file charges or report the
-          incident, you can still apply for an upgrade. The important part of
-          your application is where you explain the impact of the incident on
-          your service. For example, detail how the incident caused a decrease
-          in your productivity, or was the reason for PTSD.
-        </li>
-      )}
-      {boardToSubmit.abbr !== DRB && (
-        <li>
-          Item 16 asks for the date when you discovered the error or injustice
-          you’re asking the Board to address. If it’s been more than 3 years
-          since you found this error or injustice, you’ll need to include a
-          reason why the Board should consider your application. Examples of
-          good reasons include new evidence you’ve found to support your claim,
-          or recent changes in policy (like liberal consideration for PTSD, TBI,
-          or military sexual assault or harassment). These kinds of reasons will
-          make it more likely for the Board to decide in your favor. The 3-year
-          time limit isn’t a strict rule, so don’t let it keep you from applying
-          if you think you have a strong case.
-        </li>
-      )}
-      {boardToSubmit.abbr !== DRB && (
-        <li>
-          Item 17 asks if you’re willing to appear in person before the Board in
-          Washington, DC. The Board rarely asks Veterans to appear in person,
-          but if you say you’re willing to do so, it may help show how serious
-          you are about your case.
-        </li>
-      )}
-      {boardToSubmit.abbr === DRB &&
-        formResponses[SHORT_NAME_MAP.PREV_APPLICATION_TYPE] !==
-          RESPONSES.PREV_APPLICATION_DRB_DOCUMENTARY && (
+  const renderStepOneText = () => {
+    const DRBReviewBullets = (
+      <>
+        <p>
+          You can request either a Documentary Review or Personal Appearance
+          Review from the Discharge Review Board (DRB). Here are some tips on
+          choosing a review option:
+        </p>
+        <ul>
           <li>
-            You can request either a Documentary Review or Personal Appearance
-            Review from the Discharge Review Board (DRB). If your case is
-            especially complicated and requires detailed explanation, you may
-            have more success with a Personal Appearance Review. Note that
-            you’ll have to pay your travel costs if you make a personal
-            appearance. Documentary Reviews are generally faster, so we
-            recommend you begin with this type of review. Also, if you’re denied
-            in a Documentary Review, you can then appeal through a Personal
-            Appearance Review. But you can’t request Documentary Review after a
-            Personal Appearance Review.
+            Documentary Reviews are typically faster. We recommend starting with
+            this review first.
           </li>
-        )}
-      {boardToSubmit.abbr === DRB &&
+          <li>
+            If your case is complicated or has a lot of details, you may want to
+            choose a Personal Appearance Review. You’ll have to pay your travel
+            costs for a Personal Appearance Review.
+          </li>
+          <li>
+            If your Documentary Review is denied, you can appeal through a
+            Personal Appeal Review. You can’t request a Documentary Review after
+            a Personal Appearance Review.
+          </li>
+        </ul>
+      </>
+    );
+    if (
+      reason === RESPONSES.REASON_PTSD &&
+      (formResponses[SHORT_NAME_MAP.PREV_APPLICATION_TYPE] ===
+        RESPONSES.PREV_APPLICATION_BCNR ||
         formResponses[SHORT_NAME_MAP.PREV_APPLICATION_TYPE] ===
-          RESPONSES.PREV_APPLICATION_DRB_DOCUMENTARY && (
-          <li>
-            The DRB allows you to request either a Documentary Review or a
-            Personal Appearance Review. Because your application was already
-            denied during a Documentary Review, you must apply for a Personal
-            Appearance Review in Washington, DC. Note that you will have to pay
-            your travel costs if you make a personal appearance.
-          </li>
-        )}
-    </ul>
-  );
-
-  const { num: formNumber, link: formLink } = determineFormData(formResponses);
-  let formTitle = `Form ${formNumber}`;
-  const formFileName = formLink.split('/').pop();
-  if ([293, 149].includes(formNumber)) {
-    formTitle = `DOD Form ${formNumber}`;
-  }
-
-  const header = `Download and fill out DOD Form ${formNumber}`;
+          RESPONSES.PREV_APPLICATION_BCMR) &&
+      (formResponses[SHORT_NAME_MAP.FAILURE_TO_EXHAUST] ===
+        RESPONSES.FAILURE_TO_EXHAUST_BCNR_YES ||
+        formResponses[SHORT_NAME_MAP.FAILURE_TO_EXHAUST] ===
+          RESPONSES.FAILURE_TO_EXHAUST_BCMR_YES)
+    ) {
+      return (
+        <>
+          <p>
+            Because you’re applying for reconsideration of a previous
+            application, you’ll need to enter your previous application number
+            in Item 11b.
+          </p>
+          <p>
+            <strong>Note:</strong> You’re eligible for reconsideration if you
+            have new evidence to present that wasn’t available the last time you
+            applied. Make sure to explain exactly what that new evidence is.
+            Additionally, changes in DOD policy, like new consideration
+            guidelines for PTSD, TBI, and sexual assault or harassment, can
+            qualify you for reconsideration.
+          </p>
+          {DRBReviewBullets}
+        </>
+      );
+    }
+    if (
+      reason === RESPONSES.REASON_PTSD &&
+      formResponses[SHORT_NAME_MAP.PREV_APPLICATION] === RESPONSES.YES &&
+      formResponses[SHORT_NAME_MAP.PREV_APPLICATION_TYPE] ===
+        RESPONSES.PREV_APPLICATION_DRB_DOCUMENTARY &&
+      formResponses[SHORT_NAME_MAP.PRIOR_SERVICE] === RESPONSES.PRIOR_SERVICE_NO
+    ) {
+      return (
+        <>
+          <p>
+            Because you’re applying for reconsideration of a previous
+            application, you’ll need to enter your previous application number
+            in Item 11b.
+          </p>
+          <p>
+            Because your application was denied during a Documentary Review, you
+            must apply for a Personal Appearance Review in Washington, DC.
+            You’ll have to pay your travel costs for a Personal Appearance
+            Review.
+          </p>
+        </>
+      );
+    }
+    if (
+      reason === RESPONSES.REASON_PTSD ||
+      reason === RESPONSES.REASON_SEXUAL_ORIENTATION ||
+      reason === RESPONSES.REASON_TBI ||
+      reason === RESPONSES.REASON_UNJUST ||
+      reason === RESPONSES.REASON_ERROR
+    ) {
+      return DRBReviewBullets;
+    }
+    if (reason === RESPONSES.REASON_SEXUAL_ASSAULT) {
+      return (
+        <>
+          <p>
+            <strong>Note:</strong> Even if you didn’t file charges or report the
+            incident, you can still apply for an upgrade.
+          </p>
+          {DRBReviewBullets}
+        </>
+      );
+    }
+    if (reason === RESPONSES.REASON_TRANSGENDER) {
+      return (
+        <>
+          <p>Many Veterans attach extra pages to answer this question.</p>
+          <p>
+            In item 16, make sure to add the date you found the mistake. If it’s
+            been more than 3 years, you’ll need to include a reason why the
+            Board should consider your application. You can still apply for a
+            correction even if it’s been more than 3 years since you found the
+            mistake. You may want to share new evidence for recent changes in
+            policy that support your claim.
+          </p>
+          <p>
+            <strong>Note:</strong> In item 17, we’ll ask if you’re willing to
+            appear in person before the Board in Washington, DC. This isn’t
+            common. But it may help your case if you’re willing to appear in
+            person.
+          </p>
+        </>
+      );
+    }
+    return null;
+  };
 
   return (
     <va-process-list-item header={header} level="2">
-      <p>Important tips for completing {formTitle}:</p>
-      {formResponses[SHORT_NAME_MAP.REASON] ===
-      RESPONSES.REASON_DD215_UPDATE_TO_DD214
-        ? dd214Tips
-        : nonDd2014Tips}
+      {renderItemDetailsIntro()}
+      {renderStepOneText()}
       {/* Intentionally not using <va-link> per Platform Analytics team */}
       <va-link
         download
