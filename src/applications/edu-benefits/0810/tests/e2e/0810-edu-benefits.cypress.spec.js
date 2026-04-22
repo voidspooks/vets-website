a2623a542b0127c8b5b856c82dae3ec7d1aae741
govcioChad <162637891+govcioChad@users.noreply.github.com>
2026-04-22 10:53:04 -0400

    #vebt-2684 #comment Added utility test config for 22-0810 (#44222)
    * #vebt-2684 #comment Added utility test config for 22-0810
    
    * renamed set up test enddpoints
    
    * maximal data

--- src/applications/edu-benefits/0810/tests/e2e/0810-edu-benefits.cypress.spec.js
+++ src/applications/edu-benefits/0810/tests/e2e/0810-edu-benefits.cypress.spec.js
@@ -4,6 +4,8 @@ import { createTestConfig } from 'platform/testing/e2e/cypress/support/form-test
 import featureToggles from '../fixtures/mocks/feature-toggles.json';
 import user from '../fixtures/mocks/user.json';
 import mockSubmit from '../fixtures/mocks/application-submit.json';
+import prefilledForm from '../fixtures/mocks/prefilled-form.json';
+import sip from '../fixtures/mocks/sip-put.json';
 import formConfig from '../../config/form';
 import manifest from '../../manifest.json';
 
@@ -15,29 +17,28 @@ const testConfig = createTestConfig(
     pageHooks: {
       introduction: ({ afterHook }) => {
         afterHook(() => {
-          cy.findAllByText(/^start/i, { selector: 'a[href="#start"]' })
-            .last()
-            .click({ force: true });
+          cy.get('va-link-action[href="#start"]').click();
         });
       },
-      // Example page hook
-      // All paths are already automatically filled out based on fixtures.
-      // But if you want to manually test a page add the path.
-      // 'name-and-date-of-birth': ({ afterHook }) => {
-      //   cy.injectAxeThenAxeCheck();
-      //   afterHook(() => {
-      //     cy.get('@testData').then(() => {
-      //       cy.fillPage(); // fills all fields based on fixtures.
-      //       cy.axeCheck();
-      //       cy.findByText(/continue/i, { selector: 'button' }).click();
-      //     });
-      //   });
-      // },
+      'review-and-submit': ({ afterHook }) => {
+        afterHook(() => {
+          cy.get('va-text-input[id="veteran-signature"]').then(el => {
+            cy.fillVaTextInput(el, 'John Doe');
+          });
+          cy.get('va-checkbox[id="veteran-certify"]').then(el => {
+            cy.selectVaCheckbox(el, true);
+          });
+          cy.tabToSubmitForm();
+        });
+      },
     },
     setupPerTest: () => {
       cy.intercept('GET', '/v0/user', user);
       cy.intercept('GET', '/v0/feature_toggles?*', featureToggles);
+      cy.intercept('GET', '/data/cms/vamc-ehr.json', {});
       cy.intercept('POST', formConfig.submitUrl, mockSubmit);
+      cy.intercept('GET', '/v0/in_progress_forms/22-0810', prefilledForm);
+      cy.intercept('PUT', '/v0/in_progress_forms/22-0810', sip);
       cy.login(user);
     },
     skip: Cypress.env('CI'), // Skip CI initially until content-build is merged
